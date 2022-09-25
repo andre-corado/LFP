b92196bf479b8edcdb94a9ce558a0344b7052cbc
andre-corado <3007947540101@ingenieria.usac.edu.gt>
2022-09-25 03:48:31 -0600

    GUI FINAL

--- Proyectos/Proyecto 1/main.py
+++ Proyectos/Proyecto 1/main.py
@@ -1,9 +1,15 @@
+import os
+from pathlib import Path
+import webbrowser
+import tkinter.messagebox
 from tkinter import *
 from tkinter import filedialog
+import analizadorLexico
 
 
 class Ventana:
     def __init__(self):
+        self.editorTexto = None
         self.ventana = Tk()
         self.ventana.resizable(False, False)
         self.ventana.title('Proyecto 1 - LFP B+')
@@ -21,24 +27,25 @@ class Ventana:
         r.geometry(f'+{x}+{y}')
 
     def Ventana(self):
+        self.ruta = None
         self.menu = Menu(self.ventana.master)
         self.ventana.config(menu=self.menu)
         self.menubar = Menu(self.menu, tearoff=0, bg='#cd8b62', activebackground='#475c6c')
         # Menú Archivo
         self.archivoMenu = Menu(self.menubar, tearoff=0, bg='#f7efd2', activebackground='#475c6c')
         self.archivoMenu.add_command(label="Abrir", font=("Roboto", 25), command=self.abrir)
-        self.archivoMenu.add_command(label="Guardar", font=("Roboto", 25))
-        self.archivoMenu.add_command(label="Guardar Como", font=("Roboto", 25))
+        self.archivoMenu.add_command(label="Guardar", font=("Roboto", 25), command=self.guardar)
+        self.archivoMenu.add_command(label="Guardar Como", font=("Roboto", 25), command=self.guardarComo)
         self.archivoMenu.add_separator()
-        self.archivoMenu.add_command(label="Analizar", font=("Roboto", 25))
-        self.archivoMenu.add_command(label="Errores", font=("Roboto", 25))
+        self.archivoMenu.add_command(label="Analizar", font=("Roboto", 25), command=self.analizar)
+        self.archivoMenu.add_command(label="Errores", font=("Roboto", 25), command=self.errores)
         self.menubar.add_cascade(label="Archivo", menu=self.archivoMenu, font=("Roboto", 25))
         # Menú Ayuda
         self.ayudaMenu = Menu(self.menubar, tearoff=0, bg='#f7efd2', activebackground='#475c6c')
-        self.ayudaMenu.add_command(label="Manual de Usuario", font=("Roboto", 25))
-        self.ayudaMenu.add_command(label="Manual Técnico", font=("Roboto", 25))
+        self.ayudaMenu.add_command(label="Manual de Usuario", font=("Roboto", 25), command=self.manualDeUsuario)
+        self.ayudaMenu.add_command(label="Manual Técnico", font=("Roboto", 25), command=self.manualTecnico)
         self.ayudaMenu.add_separator()
-        self.ayudaMenu.add_command(label="Temas de Ayuda", font=("Roboto", 25))
+        self.ayudaMenu.add_command(label="Temas de Ayuda", font=("Roboto", 25), command=self.infoAutor)
         self.menubar.add_cascade(label="Ayuda", menu=self.ayudaMenu, font=("Roboto", 25))
         self.menubar.add_separator()
         self.menubar.add_command(label="Salir", command=quit, font=("Roboto", 25))
@@ -55,8 +62,69 @@ class Ventana:
     def abrir(self):
         ruta = filedialog.askopenfilename(initialdir="C:/Users/SergioLima/Downloads",
                                           title="Selecciona un archivo:",
-                                          filetypes=(("LFP files", "*.lfp*"), ("All files", "*.*")), )
+                                          filetypes=(("TXT files", "*.txt*"), ("All files", "*.*")), )
+        self.ruta = ruta
+        try:
+            f = open(ruta, 'rt')
+            text = f.read()
+            self.editorTexto.delete('1.0', END)
+            self.editorTexto.insert('1.0', text)
+            f.close()
+            self.ruta = ruta
+            ruta_dividida = ruta.split('/')
+            self.nombreArchivo.config(text=ruta_dividida[-1])
+        except:
+            tkinter.messagebox.showerror(message="No pudo leerse el archivo", title='ERROR')
 
+    def guardar(self):
+        ruta = self.ruta
+        text = self.editorTexto.get('1.0', END)
+        # Si el archivo es nuevo
+        if ruta is None:
+            self.guardarComo()
+        else:
+            try:
+                with open(ruta, "w") as f:
+                    f.write(text)
+                    f.close()
+                    self.ruta = ruta
+                    ruta_dividida = ruta.split('/')
+                    self.nombreArchivo.config(text=ruta_dividida[-1])
+
+            except FileNotFoundError:
+                tkinter.messagebox.showerror(message="No pudo leerse el archivo", title='ERROR')
+
+    def guardarComo(self):
+        text = self.editorTexto.get('1.0', END)
+        ruta = filedialog.asksaveasfilename(
+            filetypes=[("txt file", ".txt")], defaultextension=".txt")
+        f = open(ruta, 'w')
+        f.write(text)
+        f.close()
+        self.ruta = ruta
+        ruta_dividida = ruta.split('/')
+        self.nombreArchivo.config(text=ruta_dividida[-1])
+
+    def manualDeUsuario(self):
+        webbrowser.open_new('Manual De Usuario.pdf')
+
+    def manualTecnico(self):
+        webbrowser.open_new('Manual Tecnico.pdf')
+
+    def infoAutor(self):
+        tkinter.messagebox.showinfo('Información del Estudiante',
+                                    'Nombre:\nSergio André Lima Corado\n\nCarné:\n\t202100154\n'
+                                    'Sección:\n\tB+')
+
+    def analizar(self):
+        analizadorLexico.analizar(self.editorTexto.get('1.0', END))
+        ruta = Path().absolute()
+        webbrowser.open_new('file://' + str(ruta) + '/RESULTADOS_202100154.html')
+
+
+    def errores(self):
+        ruta = Path().absolute()
+        webbrowser.open_new('file://' + str(ruta) + '/ERRORES_202100154.html')
 
 if __name__ == '__main__':
     Ventana()

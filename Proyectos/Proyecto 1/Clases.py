e858f784d2711380dfaed6985a3a7a0191dbd942
andre-corado <3007947540101@ingenieria.usac.edu.gt>
2022-09-25 03:51:39 -0600

    Analizador Semántico y Léxico Básico

--- Proyectos/Proyecto 1/Clases.py
+++ Proyectos/Proyecto 1/Clases.py
@@ -1,3 +1,17 @@
+from enum import Enum
+from abc import ABC, abstractmethod
+
+
+class Expression(ABC):
+
+    def __init__(self, fila, column):
+        self.fila = fila
+        self.column = column
+
+    @abstractmethod
+    def ejecutar(self, getER):
+        pass
+
 
 class Error:
     def __init__(self, lexema, tipo, columna, fila):
@@ -8,3 +22,73 @@ class Error:
 
     def toString(self):
         return f"=======\nLexema: {self.lexema}\nTipo: {self.tipo}\nColumna: {self.columna}\nFila: {self.fila}\n======="
+
+
+class Operador(Enum):
+    OPERACION = 0
+    SUMA = 1
+    RESTA = 2
+    MULTIPLICACION = 3
+    DIVISION = 4
+    POTENCIA = 5
+    MODULO = 6
+
+
+class Generador:
+    generador = None
+
+    def getInstance(self):
+        if Generador.generador is None:
+            Generador.generador = Generador()
+        return Generador.generador
+
+    def addExpresion(self, n1, n2, tipo):
+        return f'({n1} {tipo} {n2})'
+
+
+class Aritmeticas(Expression):
+
+    def __init__(self, left, right, tipo, fila, column):
+        self.left = left
+        self.right = right
+        self.tipo = tipo
+        super().__init__(fila, column)
+
+    def ejecutar(self, getER):
+        genAux = Generador()
+        generador = genAux.getInstance()
+
+        izq = self.left.ejecutar(getER)
+        der = self.right.ejecutar(getER)
+        if self.tipo == Operador.SUMA:
+            return generador.addExpresion(izq, der, '+') if getER else izq + der
+        elif self.tipo == Operador.RESTA:
+            return generador.addExpresion(izq, der, '-') if getER else izq - der
+            # return izq - der
+        elif self.tipo == Operador.MULTIPLICACION:
+            return generador.addExpresion(izq, der, '*') if getER else izq * der
+            # return izq * der
+        elif self.tipo == Operador.DIVISION:
+            if der != 0:
+                return generador.addExpresion(izq, der, '/') if getER else izq / der
+                # return izq / der
+            else:
+                print("Error: Division por cero")
+                return None
+        elif self.tipo == Operador.POTENCIA:
+            return generador.addExpresion(izq, der, '^') if getER else izq ** der
+            # return izq ** der
+        elif self.tipo == Operador.MODULO:
+            return generador.addExpresion(izq, der, '%') if getER else izq % der
+            # return izq % der
+        else:
+            return 0
+
+class Numero(Expression):
+
+    def __init__(self, valor, fila, column):
+        self.valor = valor
+        super().__init__(fila, column)
+
+    def ejecutar(self, getER):
+        return self.valor
--- Proyectos/Proyecto 1/analizadorLexico.py
+++ Proyectos/Proyecto 1/analizadorLexico.py
@@ -1,67 +1,42 @@
 import ply.lex as lex
-import re
-import codecs
-import os
-import sys
-from Clases import Error
+from Clases import *
 
 # Tokens
-tokens = ['RESTILO', 'ROPERACIONES', 'RTIPO',
-          'RTEXTO', 'RTIPO2', 'RTEXTO2',
-          'RFUNCION', 'RTITULO', 'RDESCRIPCION',
-          'RCONTENIDO', 'ROPERACION', 'RCOLOR',
-          'RTAMANIO', 'RNUMERO', 'RSUMA',
-          'RRESTA', 'RMULTIPLICACION', 'RDIVISION',
-          'RINVERSO', 'RESCRIBIR', 'LLAA', 'LLAC',
-          'IGUAL', 'DIV', 'ENTERO',
-          'DECIMAL', 'CADENA', 'CORA',
-          'CORC', 'RAZUL', 'RVERDE', 'RROJO',
-          'RNEGRO', 'RBLANCO', 'RGRIS',
-          'RCELESTE', 'RCAFE'
-          ]
+tokens = (
+    'RTIPO',
+    'ROPERACION',
+    'RNUMERO',
+    'RSUMA',
+    'RRESTA',
+    'RMULTIPLICACION',
+    'RDIVISION',
+    'LLAA',
+    'LLAC',
+    'IGUAL',
+    'DIV',
+    'ENTERO',
+    'DECIMAL',
+)
 
 # Lexemas
-t_IGNORAR = ' \t'
-t_RESTILO = r'Estilo'
-t_RTIPO = r'Tipo'
-t_RTEXTO = r'Texto'
-t_RTIPO2 = r'TIPO'
-t_RTEXTO2 = r'TEXTO'
-t_RFUNCION = r'Funcion'
-t_RTITULO = r'Titulo'
-t_RDESCRIPCION = r'Descripcion'
-t_RCONTENIDO = r'Contenido'
-t_ROPERACION = r'Operacion'
-t_ROPERACIONES = r'Operaciones'
-t_RCOLOR = r'Color'
-t_RTAMANIO = r'Tamanio'
-t_RSUMA = r'SUMA'
-t_RRESTA = r'RESTA'
+t_ignore = ' \t'
+t_RTIPO       = r'Tipo'
+t_ROPERACION  = r'Operacion'
+t_RSUMA       = r'SUMA'
+t_RRESTA       = r'RESTA'
 t_RMULTIPLICACION = r'MULTIPLICACION'
 t_RDIVISION = r'DIVISION'
-t_RINVERSO = r'INVERSO'
-t_RESCRIBIR = r'ESCRIBIR'
-t_RNUMERO = r'Numero'
-t_LLAA = r'<'
-t_LLAC = r'>'
-t_IGUAL = r'='
-t_DIV = r'/'
-t_CORA = r'\['
-t_CORC = r'\]'
-t_RAZUL = r'AZUL'
-t_RVERDE = r'VERDE'
-t_RROJO = r'ROJO'
-t_RNEGRO = r'NEGRO'
-t_RBLANCO = r'BLANCO'
-t_RGRIS = r'GRIS'
-t_RCELESTE = r'CELESTE'
-t_RCAFE = r'CAFE'
+t_RNUMERO     = r'Numero'
+t_LLAA        = r'<'
+t_LLAC        = r'>'
+t_IGUAL       = r'='
+t_DIV         = r'/'
 
 
 # Gramática para números
 
 def t_DECIMAL(t):
-    r'\d+\-\d+'
+    r'\d+\.\d+'
     try:
         t.value = float(t.value)
     except ValueError:
@@ -80,7 +55,7 @@ def t_ENTERO(t):
     return t
 
 
-def t_nuevaLinea(t):
+def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
 
@@ -105,9 +80,101 @@ def find_column(inp, tk):
 # Analizador léxico
 lexer = lex.lex()
 
-archivo = open('analizador.txt', 'r')
-global input
-global errores_
-errores_ = []
-input = archivo.read()
-archivo.close()
+
+
+# ANALIZADOR SINTACTICO
+# Definicion de la gramatica
+
+def p_init(t):
+    'init : instrucciones'
+    t[0] = t[1]
+    return t[0]
+
+def p_instrucciones_lista(t):
+    '''instrucciones    : instrucciones instruccion
+                        |   instruccion'''
+    if len(t) == 2:
+        t[0] = [t[1]]
+    else:
+        t[1].append(t[2])
+        t[0] = t[1]
+
+def p_instruccion(t):
+    'instruccion  : LLAA RTIPO LLAC instrucciones_2 LLAA DIV RTIPO LLAC'
+    t[0] = t[4]
+
+def p_instrucciones_2_lista(t):
+    'instrucciones_2 : instrucciones_2 instruccion_2'
+    t[1].append(t[2])
+    t[0] = t[1]
+
+def p_instrucciones_2_instruccion(t):
+    'instrucciones_2 :  instruccion_2'
+    t[0] = [t[1]]
+
+def p_instruccion_2(t):
+    'instruccion_2  :  LLAA ROPERACION IGUAL tipo LLAC instrucciones_2 LLAA DIV ROPERACION LLAC'
+    t[0] = Aritmeticas(t[6][0], t[6][1], t[4], t.lineno(1), find_column(input,t.slice[1]))
+
+def p_instruccion_2_decimal(t):
+    'instruccion_2 : LLAA RNUMERO LLAC DECIMAL LLAA DIV RNUMERO LLAC '
+    t[0] = Numero(float(t[4]), t.lineno(1), find_column(input,t.slice[1]))
+
+def p_instruccion_2_entero(t):
+    'instruccion_2 : LLAA RNUMERO LLAC ENTERO LLAA DIV RNUMERO LLAC '
+    t[0] = Numero(int(t[4]), t.lineno(1), find_column(input,t.slice[1]))
+
+def p_tipo(t):
+    '''tipo :   RSUMA
+            |   RRESTA
+            |   RMULTIPLICACION
+            |   RDIVISION
+    '''
+    if t[1] == 'SUMA':
+        t[0] = Operador.SUMA
+    elif t[1] == 'RESTA':
+        t[0] = Operador.RESTA
+    elif t[1] == 'MULTIPLICACION':
+        t[0] = Operador.MULTIPLICACION
+    elif t[1] == 'DIVISION':
+        t[0] = Operador.DIVISION
+
+# Aqui reconoce un error de sintaxis, pueden crear un array e irlos agregando
+# para obtenerlos después
+def p_error(t):
+    print("Error de sintaxis en '%s'" % t.value)
+
+# Esta función busca la columna en la que se encuentra el token o lexema
+def find_column(inp, tk):
+    try:
+        line_start = inp.rfind('\n',0,tk.lexpos) + 1
+        return (tk.lexpos - line_start) + 1
+    except:
+        return 0
+
+import ply.yacc as yacc
+parser = yacc.yacc()
+
+def parse(input):
+    lexer.lineno = 1
+    return parser.parse(input)
+
+genAux = Generador()
+generador = genAux.getInstance()
+
+
+def analizar(texto):
+    global input
+    global errores_
+    errores_ = []
+    input = texto
+    variable = parse(input)
+    getER = True
+
+    for var in variable:
+        for var_ in var:
+            print(var_.ejecutar(getER))
+
+    for var in errores_:
+        print(var.toString())
+
